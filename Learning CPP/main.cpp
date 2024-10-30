1d7d4580a16d253b42915c287ca4de30484e614c
Moballighul Islam <moballigh5225@gmail.com>
2024-10-30 23:09:39 +0530

    Initial Commit

---
+++ Learning CPP/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Learning CPP
+//
+//  Created by Moballighul Islam on 30/10/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
