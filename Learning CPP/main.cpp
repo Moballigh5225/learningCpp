4defdd1217934907417a55fbddd146e6cca05770
Moballighul Islam <moballigh5225@gmail.com>
2024-10-30 23:17:46 +0530

    feat: add primer EXERCISES SECTION 1.2 - Exercise 1.4

--- Learning CPP/main.cpp
+++ Learning CPP/main.cpp
@@ -3,12 +3,14 @@
 //  Learning CPP
 //
 //  Created by Moballighul Islam on 30/10/24.
-//
+//Exercise 1.4: Our program used the addition operator, +, to add two numbers.
 
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main(){
+    std:: cout << "program to add two integer variables" << std::endl;
+    int v1 = 10, v2 = 20;
+    std:: cout << "output is "<< std::endl;
+    std::cout << v1 + v2 ;
+    
     return 0;
 }
